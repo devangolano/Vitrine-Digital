dd05f435a0388dd1ba8cfc24431dd5a69df649a8
Dev-Angolano <albertoronny237@gmail.com>
2024-04-28 19:32:28 +0100

    Exibição do modal de pedidos

--- main.js
+++ main.js
@@ -1,8 +1,21 @@
+const menu = document.getElementById('menu');
 const modal = document.getElementById('cart-modal');
+const cartBtn = document.getElementById('cart-btn')
+const cratItemsContainer = document.getElementById('cart-items')
+const cartTotal = document.getElementById('cart-total')
+const checkoutBtn = document.getElementById('checkout-btn')
+const cartCount = document.getElementById('cart-count')
+const adressInput = document.getElementById('address')
+const adressWarn = document.getElementById('address-warn')
 
-function mostrarElemento() {
-    if (modal) {
-        modal.classList.remove('hidden');
-        modal.style.display = 'flex';
+
+
+cartBtn.addEventListener("click", function() {
+    modal.style.display = "flex"
+})
+
+modal.addEventListener('click', function (event) {
+    if (event.target === modal) {
+        modal.style.display = 'none'
     }
-}
+})
